bc60ce4cc2c599c6c41416d72a730e1fa81c0abd
eniv <ainvyu@gmail.com>
2022-08-28 10:03:06 +0900

    Update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors, UsePipes, ValidationPipe } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { IsEmail, IsMobilePhone } from 'class-validator';
 import { AppService } from './app.service';
+
+class RecruitSubmissionDto {
+  name: string;
+
+  @IsMobilePhone('ko-KR')
+  phone: string;
+
+  @IsEmail()
+  email: string;
+}
 
 @Controller()
 export class AppController {
@@ -11,9 +22,17 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @UsePipes(new ValidationPipe({ enableDebugMessages: true }))
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File, @Body() body): string {
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addMaxSizeValidator({ maxSize: 1024*1024*100 }) // <= 100MB
+        .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY }),
+    ) file: Express.Multer.File, 
+    @Body() body: RecruitSubmissionDto
+  ): string {
     console.log(file);
     console.log(body);
 
